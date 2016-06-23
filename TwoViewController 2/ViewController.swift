0f40118e293fe6dd176635eb0ba038b361bb4971
Chanidapha <peedee777888@gmail.com>
2016-06-23 09:57:34 +0700

    Initial Commit

---
+++ TwoViewController 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwoViewController 2
+//
+//  Created by iMac_14 on 6/23/2559 BE.
+//  Copyright © 2559 Chanidapha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
